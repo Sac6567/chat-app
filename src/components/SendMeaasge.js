b25c9df699974fed55cfa86a63f77b1e66da972e
Sac6567 <sac6567@gmail.com>
2022-09-19 16:21:07 +0530

    Chat App

---
+++ src/components/SendMeaasge.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+const style = {
+  form: `h-14 w-full max-w-[728px]  flex text-xl absolute bottom-0`,
+  input: `w-full text-xl p-3 bg-gray-900 text-white outline-none border-none`,
+  button: `w-[20%] bg-green-500`,
+};
+
+const SendMeaasge = ({ scroll }) => {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (ev) => {
+    ev.preventDefault();
+    if (input === "") {
+      alert("Enter a message");
+      return;
+    }
+    const { uid, displayName } = auth.currentUser;
+    await addDoc(collection(db, "messages"), {
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: serverTimestamp(),
+    });
+    setInput("");
+    scroll.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <form onSubmit={sendMessage} className={style.form}>
+      <input
+        className={style.input}
+        onChange={(ev) => setInput(ev.target.value)}
+        type="text"
+        placeholder="Message"
+      />
+      <button className={style.button} type="submit">
+        Send
+      </button>
+    </form>
+  );
+};
+
+export default SendMeaasge;
